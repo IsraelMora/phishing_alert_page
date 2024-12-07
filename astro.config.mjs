43d732e96369e804850e61021a50fcc61b071bf8
Israel <isramora3@gmail.com>
2024-12-06 22:01:36 -0400

    deploy in github

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://IsraelMora.github.io',
+  base: 'phishing_alert_page',
   integrations: [tailwind()]
 });
